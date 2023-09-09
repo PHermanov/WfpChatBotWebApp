90a2697aa03ed79f6b033b9dc9543210fad4350e
PHermanov <germanovpavel@gmail.com>
2023-09-09 09:56:43 +0300

    fix sql

--- sql/CreateTableResults.sql
+++ sql/CreateTableResults.sql
@@ -2,6 +2,6 @@ CREATE TABLE `results` (
   `chatid` int NOT NULL,
   `userid` int NOT NULL,
   `playedat` date NOT NULL,
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- sql/CreateTableUsers.sql
+++ sql/CreateTableUsers.sql
@@ -3,6 +3,6 @@ CREATE TABLE `users` (
   `userid` int NOT NULL,
   `username` text,
   `inactive` tinyint NOT NULL DEFAULT '0',
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
