4bd176dfb2b35bfccc508cf1949b2127b1bcb9b1
PHermanov <germanovpavel@gmail.com>
2024-06-24 13:38:13 +0300

    rebuild fucntions

--- Functions/WfpFunctions/DailyWinnerFunction.cs
+++ Functions/WfpFunctions/DailyWinnerFunction.cs
@@ -3,27 +3,26 @@ using System.Net.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace GameAzureFunctions;
+namespace WfpFunctions;
 
-public class SelectDayWinnerFunction
+public class DailyWinnerFunction
 {
     private readonly HttpClient _httpClient;
     private readonly IConfiguration _configuration;
 
-    public SelectDayWinnerFunction(IConfiguration configuration, HttpClient httpClient)
+    public DailyWinnerFunction(IConfiguration configuration, HttpClient httpClient)
     {
         _configuration = configuration;
         _httpClient = httpClient;
     }
 
-    [FunctionName("SelectDayWinner")]
-    public void Run([TimerTrigger("0 30 12 * * *")] TimerInfo myTimer, ILogger log)
+    [FunctionName("DailyWinnerFunction")]
+
+    public void Run([TimerTrigger("0 35 13 * * *")]TimerInfo myTimer, ILogger log)
     {
-        log.LogInformation($"{nameof(SelectDayWinnerFunction)} function executed at: {DateTime.Now}");
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         var hostAddress = _configuration.GetValue<string>("HostAddress");
         log.LogInformation(hostAddress + "123");
     }
 }
-
--- Functions/WfpFunctions/Startup.cs
+++ Functions/WfpFunctions/Startup.cs
@@ -4,23 +4,23 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
-[assembly: FunctionsStartup(typeof(GameAzureFunctions.Startup))]
+[assembly: FunctionsStartup(typeof(WfpFunctions.Startup))]
 
-namespace GameAzureFunctions;
+namespace WfpFunctions;
 
 public class Startup : FunctionsStartup
 {
     public override void Configure(IFunctionsHostBuilder builder)
     {
         //var executionContextOptions = builder.Services.BuildServiceProvider()
-         //   .GetService<IOptions<ExecutionContextOptions>>().Value;
+        //   .GetService<IOptions<ExecutionContextOptions>>().Value;
         //var appDirectory = executionContextOptions.AppDirectory;
 
         var config = new ConfigurationBuilder()
             //.SetBasePath(appDirectory)
             //.AddJsonFile(Path.Combine(appDirectory, "settings.json"), optional: true, reloadOnChange: true)
             .AddAzureKeyVault(new Uri("https://wfpbotkeyvault.vault.azure.net/"), new DefaultAzureCredential());
-        
+
         builder.Services.AddHttpClient();
     }
 }
