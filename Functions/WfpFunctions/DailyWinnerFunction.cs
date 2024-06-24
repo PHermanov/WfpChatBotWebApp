fb52bdc87abb127659a6d62c8527930e838049bd
PHermanov <germanovpavel@gmail.com>
2024-06-24 15:48:56 +0300

    remove startup from functions

--- Functions/WfpFunctions/DailyWinnerFunction.cs
+++ Functions/WfpFunctions/DailyWinnerFunction.cs
@@ -1,28 +1,28 @@
 using System;
 using System.Net.Http;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace WfpFunctions;
 
 public class DailyWinnerFunction
 {
-    private readonly HttpClient _httpClient;
-
-    public DailyWinnerFunction(IConfiguration configuration, HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-        _httpClient.BaseAddress = new Uri(configuration.GetValue<string>("HostAddress"));
-    }
-
     [FunctionName("DailyWinnerFunction")]
-    public void Run([TimerTrigger("0 25 12 * * *")]TimerInfo myTimer, ILogger log)
+    public void Run([TimerTrigger("0 52 12 * * *")]TimerInfo myTimer, ILogger log)
     {
         log.LogInformation("DailyWinner function executed at: {Now}", DateTime.UtcNow);
 
-        var response = _httpClient.PostAsync("job/daily", null).Result;
+        var hostAdress = Environment.GetEnvironmentVariable("HostAddress", EnvironmentVariableTarget.Process);
+
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(hostAdress)
+        };
+
+        var response = httpClient.PostAsync("job/daily", null).Result;
         
         log.LogInformation("DailyWinner received response: {Code}, {Reason}", response.StatusCode, response.ReasonPhrase);
+
+        httpClient.Dispose();
     }
 }
