e0fa6aee063a8338852f0f285b6051a91ca519c6
PHermanov <germanovpavel@gmail.com>
2024-07-08 18:45:59 +0300

    add test project to play with image drawing

---
+++ Tools/PictureProcessor/PictureProcessor/PictureProcessor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+var imageStream = await PictureProcessor.PictureProcessor.GetWinnerImageMonth();
+
+var fileName = $"test_{DateTime.Now:dd-MM-HH-mm-ss}.png";
+
+var fileStream = File.Create(fileName);
+imageStream.WriteTo(fileStream);
+fileStream.Close();
+imageStream.Close();
+
+
+
+
