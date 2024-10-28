1f85c08943292a6f410aea5c7374aa8ba9edc1b7
PHermanov <germanovpavel@gmail.com>
2024-10-28 18:12:31 +0200

    add ping controller

---
+++ WfpChatBotWebApp/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WfpChatBotWebApp.Controllers;
+
+[ApiController]
+[Route("/")]
+public class PingController(ILogger<PingController> logger) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        logger.LogInformation("Ping received");
+        return Ok();
+    }
+}
