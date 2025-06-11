f1a54324e749faaf1d15e6755e325c1192d06067
Yevhen Poliakov <poliakov.yevhen@gmail.com>
2025-06-11 12:28:17 +0200

    Generate gif instead of using static sticker

--- WfpChatBotWebApp/TelegramBot/Jobs/WednesdayJob.cs
+++ WfpChatBotWebApp/TelegramBot/Jobs/WednesdayJob.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using WfpChatBotWebApp.Persistence;
 using WfpChatBotWebApp.TelegramBot.Extensions;
 using WfpChatBotWebApp.TelegramBot.Services;
@@ -12,11 +13,17 @@ public class WednesdayJobRequest : IRequest;
 public class WednesdayJobHandler(
     ITelegramBotClient botClient,
     IGameRepository repository,
-    IStickerService stickerService,
+    ISoraService soraService,
     ITextMessageService messageService,
     ILogger<MonthlyWinnerJobRequest> logger)
     : IRequestHandler<WednesdayJobRequest>
 {
+    private const string PromptTemplate = "«It’s wednesday my dudes» text. wednesday frog. {0} theme. {1}.";
+
+    private readonly string[] _themes = ["mexican", "forest", "beach", "industrial", "traditional", "anime", "fantasy"];
+
+    private readonly string[] _styles = ["photorealistic", "cartoon", "news broadcast"];
+
     public async Task Handle(WednesdayJobRequest request, CancellationToken cancellationToken)
     {
         logger.LogInformation("{Name} at {Now}", nameof(WednesdayJobHandler), DateTime.UtcNow);
@@ -24,7 +31,8 @@ public class WednesdayJobHandler(
         try
         {
             var allChatIds = await repository.GetAllChatsIdsAsync(cancellationToken);
-            logger.LogInformation("{Name} For chats: {Chats} ", nameof(WednesdayJobHandler), string.Join(',', allChatIds));
+            logger.LogInformation("{Name} For chats: {Chats} ", nameof(WednesdayJobHandler),
+                string.Join(',', allChatIds));
 
             if (allChatIds.Length == 0)
             {
@@ -32,36 +40,41 @@ public class WednesdayJobHandler(
                 return;
             }
 
-            var message = await messageService.GetMessageByNameAsync(TextMessageService.TextMessageNames.WednesdayMyDudes, cancellationToken);
+            var message =
+                await messageService.GetMessageByNameAsync(TextMessageService.TextMessageNames.WednesdayMyDudes,
+                    cancellationToken);
             if (string.IsNullOrWhiteSpace(message))
             {
                 logger.LogError("{Name}, message template not found", nameof(WednesdayJobHandler));
                 return;
             }
 
-            for (var i = 0; i < allChatIds.Length; i++)
+            foreach (var chatId in allChatIds)
             {
-                try
+                var prompt = string.Format(
+                    PromptTemplate,
+                    _themes[new Random().Next(_themes.Length)],
+                    _styles[new Random().Next(_styles.Length)]);
+
+                var stream = await soraService.GetVideo(prompt, 5, cancellationToken);
+
+                if (stream is not null)
                 {
-                    await botClient.TrySendTextMessageAsync(
-                        chatId: allChatIds[i],
-                        text: message,
-                        logger: logger,
-                        cancellationToken: cancellationToken);
-
-                    var stickerUrl = await stickerService.GetRandomStickerFromSet(StickerService.StickerSet.Frog, cancellationToken);
-                    if (string.IsNullOrWhiteSpace(stickerUrl))
-                        return;
-
-                    await botClient.TrySendStickerAsync(
-                        chatId: allChatIds[i],
-                        sticker: InputFile.FromUri(stickerUrl),
+                    await botClient.TrySendAnimationAsync(
+                        chatId: chatId,
+                        video: InputFile.FromStream(stream, "file.mp4"),
+                        caption: message,
                         logger: logger,
                         cancellationToken: cancellationToken);
                 }
-                catch (Exception e)
+                else
                 {
-                    logger.LogError(e, "Exception  in {Name} for {ChatId}", nameof(WednesdayJobHandler), allChatIds[i]);
+                    await botClient.TrySendTextMessageAsync(
+                        chatId: chatId,
+                        text: message,
+                        parseMode: ParseMode.Markdown,
+                        logger: logger,
+                        cancellationToken: cancellationToken);
                 }
             }
         }
