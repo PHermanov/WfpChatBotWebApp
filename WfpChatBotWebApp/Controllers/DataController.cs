f7dfc812d3828093020bcdaa82e7abce6ea9c083
Pavlo Hermanov <germanovpavel@gmail.com>
2025-10-04 11:27:57 +0300

    data-controlle-chats (#7)
    * update packages
    
    * add chats endpoint

---
+++ WfpChatBotWebApp/Controllers/DataController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WfpChatBotWebApp.Persistence;
+
+namespace WfpChatBotWebApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DataController(IGameRepository repository, ILogger<DataController> logger) : ControllerBase
+{
+    [HttpGet("chats")]
+    public async Task<IActionResult> GetAllChats(
+        [FromQuery] string secret,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var users = await repository.GetAllChatsIdsAsync(cancellationToken);
+
+            if (users.Length > 0)
+                return Ok(users);
+            
+            return NotFound();
+        }
+        catch (Exception e)
+        {
+            logger.LogError("Exception in {className}: {message}", nameof(DataController), e.Message);
+            return Problem();
+        }
+    }
+}
